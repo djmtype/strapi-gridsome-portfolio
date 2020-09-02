eaeb13a63fe09f6f12b5171e1f07e9f8c5a0afac
Scott Rod <djmtype@gmail.com>
2020-09-02 11:17:49 -0400

    update

--- backend/extensions/upload/config/settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  provider: 'cloudinary',
-  providerOptions: {
-    cloud_name: 'mtype-media',
-    api_key: '364748361939745',
-    api_secret: 'QyM1H0BIzNXJhGMtK8JA_pb0X80',
-  },
-}
